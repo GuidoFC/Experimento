e19bca2f0fff058abe3d8995570761e6f3239126
GuidoFC <guidofigueroa96@gmail.com>
2024-02-02 18:20:07 +0100

    haciendo una prueba para ver si lo hago bien

---
+++ Borrar 11 - encapsulamiento y constructor/encapsulamientoYconstructor/src/DateTwoTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+
+public class DateTwoTest {
+    public static void main(String args[]) {
+
+
+        /*
+        El objetivo de esta práctica es ver que no se puede usar los atributos de una clase
+        si estos atritubos son PRIVATE
+        DateTwo prueba2 = new DateTwo();
+        prueba2.day = 12;
+        prueba2.month = 05;
+        prueba2.year = 1980;
+
+        System.out.println("Fecha de nacimiento: " + prueba2.day +"/" + prueba2.month +"/"+ prueba2.year);
+        */
+
+    }// end main
+
+} // end class
+
